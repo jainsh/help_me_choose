f633f264559e1a20e0481cc2710fbd7322aaab01
Sharad Jain <sjain@connectedhealth.com>
2014-05-18 13:06:43 -0500

    allow a person to edit profile

--- app/controllers/usage_profiles_controller.rb
+++ app/controllers/usage_profiles_controller.rb
@@ -1,4 +1,5 @@
 class UsageProfilesController < ApplicationController
+
   def new
     @usage_profile = UsageProfile.new
     ServiceCost.find_each do |sc|
@@ -15,10 +16,24 @@ class UsageProfilesController < ApplicationController
     end
   end
 
+  def edit
+    @usage_profile = UsageProfile.find(params[:id])
+    render :new
+  end
+
+  def update
+    @usage_profile = UsageProfile.find(params[:id])
+    if @usage_profile.update_attributes(usage_profile_params)
+      redirect_to plan_comparison_path(@usage_profile)
+    else
+      render :new
+    end
+  end
+
   private
 
   def usage_profile_params
-    params.require(:usage_profile).permit(:gender, :zipcode, :date_of_birth, service_usages_attributes: [ :service_cost_id, :usage_count ])
+    params.require(:usage_profile).permit(:id, :gender, :zipcode, :date_of_birth, service_usages_attributes: [ :id, :service_cost_id, :usage_count ])
   end
 
 end
