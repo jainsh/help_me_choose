68a92e0835b671daec5af4bcdec4c4d32f25f59c
Kevin Musiorski <kmusiorski@connectedhealth.com>
2014-05-17 20:29:44 -0500

    * creates usage_profile

--- app/controllers/usage_profiles_controller.rb
+++ app/controllers/usage_profiles_controller.rb
@@ -5,4 +5,20 @@ class UsageProfilesController < ApplicationController
       @usage_profile.service_usages.build(:service_cost => sc)
     end
   end
+
+  def create
+    @usage_profile = UsageProfile.new(usage_profile_params)
+    if @usage_profile.save
+      redirect_to service_costs_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def usage_profile_params
+    params.require(:usage_profile).permit(:gender, :zipcode, :date_of_birth, service_usages_attributes: [ :usage_count ])
+  end
+
 end
