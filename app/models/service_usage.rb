8286fcc8ffa05583da11cb25abac751ce6b9b584
Kevin Musiorski <kmusiorski@connectedhealth.com>
2014-05-17 22:14:06 -0500

    * allow blanks * fix gender input

--- app/models/service_usage.rb
+++ app/models/service_usage.rb
@@ -14,7 +14,7 @@ class ServiceUsage < ActiveRecord::Base
   belongs_to :service_cost
   belongs_to :usage_profile
 
-  validates :usage_count, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
+  validates :usage_count, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0, :allow_blank => true }
 
   def benefit_for(plan)
     PlanBenefit.where(plan_id: plan.id, service_cost_id: service_cost.id).first
